c137d0cc88eccdf87d100aa710dcb18e84ee2458
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-10-01 12:55:57 +0600

    full file setup done..

---
+++ app/Models/BaseModel.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class BaseModel extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /* ================================================================
+     * *** PROPERTIES ***
+     ================================================================ */
+
+    protected $appends = [
+        'created_at_human',
+        'updated_at_human',
+        'deleted_at_human',
+
+        'created_at_formatted',
+        'updated_at_formatted',
+        'deleted_at_formatted',
+    ];
+
+    /* ================================================================
+     * *** Relations ***
+     ================================================================ */
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by')->select('id', 'name');
+    }
+
+    public function updatedBy()
+    {
+        return $this->belongsTo(User::class, 'updated_by')->select('id', 'name');
+    }
+
+    public function deletedBy()
+    {
+        return $this->belongsTo(User::class, 'deleted_by')->select('id', 'name');
+    }
+
+    /* ================================================================
+     * *** Accessors ***
+     ================================================================ */
+
+    public function getCreatedAtHumanAttribute(): string
+    {
+        return $this->created_at->diffForHumans();
+    }
+
+    public function getUpdatedAtHumanAttribute(): string
+    {
+        return $this->updated_at->diffForHumans();
+    }
+
+    public function getDeletedAtHumanAttribute(): string
+    {
+        return $this->deleted_at->diffForHumans();
+    }
+
+    public function getCreatedAtFormattedAttribute(): string
+    {
+        return Carbon::parse($this->created_at)->format('d M, Y h:i A');
+    }
+
+    public function getUpdatedAtFormattedAttribute(): string
+    {
+        return Carbon::parse($this->updated_at)->format('d M, Y h:i A');
+    }
+
+    public function getDeletedAtFormattedAttribute(): string
+    {
+        return Carbon::parse($this->deleted_at)->format('d M, Y h:i A');
+    }
+}
--- routes/user.php
+++ routes/user.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Livewire\Backend\User\Profile;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->name('user.')->prefix('user')->group(function () {
-    //
+    Route::get('/profile', Profile::class)->name('profile');
 });
