9b316e7a391e985c582ff90e538bad5fab657129
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-10-02 15:37:24 +0600

    my booking components add

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -80,9 +80,9 @@
                             <div class="container mx-auto px-1 xs:px-4 pt-8">
                                 <div class="flex gap-4 mb-8">
                                     <!-- My Bookings -->
-                                    <a href="{{ route('user.dashboard') }}" wire:navigate
+                                    <a href="{{ route('user.my-bookings') }}" wire:navigate
                                         class="flex-1 text-center lg:text-xl xs:text-base text-sm py-3 px-3 xs:px-6 rounded-lg font-medium transition-colors duration-200 hover:opacity-90 
-                                            {{ $page_slug === 'user-dashboard' ? 'bg-zinc-500 text-white' : 'bg-gray-200 text-gray-600' }}">
+                                            {{ $page_slug === 'user-my-bookings' ? 'bg-zinc-500 text-white' : 'bg-gray-200 text-gray-600' }}">
                                         My Bookings
                                     </a>
 
--- routes/user.php
+++ routes/user.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Livewire\Backend\User\Dashboard;
+use App\Livewire\Backend\User\MyBookings;
 use App\Livewire\Backend\User\Profile;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->name('user.')->prefix('user')->group(function () {
     Route::get('/profile', Profile::class)->name('profile');
     Route::get('/dashboard', Dashboard::class)->name('dashboard');
+    Route::get('/my-bookings', MyBookings::class)->name('my-bookings');
 });
