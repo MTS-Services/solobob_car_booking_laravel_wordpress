9a207dcf2c6d90934536e3b24e2a9ac8a03485e4
Md Monirul Islam Taluqdar <82707883+monirulislam0@users.noreply.github.com>
2025-10-04 17:50:14 +0600

    Update OrderComponent.php

--- app/Livewire/Backend/Admin/OrderManagement/OrderComponent.php
+++ app/Livewire/Backend/Admin/OrderManagement/OrderComponent.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Backend\Admin\OrderManagement;
 
 use Livewire\Component;
 use Livewire\Attributes\Layout;
+use App\Models\Booking;
 
 #[Layout(
     'app',
@@ -24,7 +25,18 @@ class OrderComponent extends Component
 
     public function render()
     {
-        $orders = []; // Replace with actual order fetching logic
+        $orders = Booking::query()
+            ->when($this->search, function ($query) {
+                $query->where(function ($q) {
+                    $q->where('name', 'like', '%' . $this->search . '%')
+                        ->orWhere('slug', 'like', '%' . $this->search . '%');
+                });
+            })
+            ->with(['vehicle', 'user','pickupLocation','auditor'])
+            ->latest()
+            ->paginate(10);
+
+       
         return view('livewire.backend.admin.order-management.order-component', compact('orders'));
     }
 }
