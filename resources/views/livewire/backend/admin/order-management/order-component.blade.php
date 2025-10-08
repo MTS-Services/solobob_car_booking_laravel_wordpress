05f3123e0ad76ae4fcabe5a974f6c84956c269e0
Md Monirul Islam Taluqdar <82707883+monirulislam0@users.noreply.github.com>
2025-10-08 15:10:56 +0600

    Trashed Removed
    Trashed Removed

--- resources/views/livewire/backend/admin/order-management/order-component.blade.php
+++ resources/views/livewire/backend/admin/order-management/order-component.blade.php
@@ -4,9 +4,7 @@
         <div class="flex items-center justify-between glass-card rounded-2xl p-6 mb-6">
             <h2 class="text-xl font-bold text-text-primary">{{ __('Order List') }}</h2>
             <div class="flex items-center gap-2">
-                {{-- <x-button href="#" icon="trash-2" type='secondary' permission="order-trash">
-                    {{ __('Trash') }}
-                </x-button>
+                {{-- 
                 <x-button href="#" icon="plus" permission="order-create">
                     {{ __('Add') }}
                 </x-button> --}}
