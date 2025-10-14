438b7c9f9721de00028db483c8567f382dda2ead
Rashed Miya <132655219+rashedsmiya@users.noreply.github.com>
2025-10-14 18:45:17 +0600

    add_more

--- routes/admin.php
+++ routes/admin.php
@@ -1,34 +1,29 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
+use App\Livewire\Backend\Admin\AdminManagement\Admin;
 // Admin Components
 use App\Livewire\Backend\Admin\Dashboard;
-use App\Livewire\Backend\Admin\Profile;
-use App\Livewire\Backend\Admin\AdminManagement\Admin;
-use App\Livewire\Backend\Admin\UserManagement\User;
-
-// Product Management Components
-use App\Livewire\Backend\Admin\ProductManagement\ProductCategory;
-use App\Livewire\Backend\Admin\ProductManagement\Vehiclefuels;
-use App\Livewire\Backend\Admin\ProductManagement\VehicleMakes;
-use App\Livewire\Backend\Admin\ProductManagement\VehicleModels;
-use App\Livewire\Backend\Admin\ProductManagement\Vehicles;
-use App\Livewire\Backend\Admin\ProductManagement\VehicleTransmissions;
-
-// Other Management Components
 use App\Livewire\Backend\Admin\DepositManagement\DepositComponent;
 use App\Livewire\Backend\Admin\DepositManagement\DepositDetail;
 use App\Livewire\Backend\Admin\OrderManagement\OrderComponent;
+// Product Management Components
 use App\Livewire\Backend\Admin\OrderManagement\OrderDetails;
 use App\Livewire\Backend\Admin\PaymentManagement\PaymentComponent;
 use App\Livewire\Backend\Admin\PaymentManagement\PaymentDetailsComponent;
+use App\Livewire\Backend\Admin\ProductManagement\ProductCategory;
+use App\Livewire\Backend\Admin\ProductManagement\Vehiclefuels;
+use App\Livewire\Backend\Admin\ProductManagement\VehicleMakes;
+// Other Management Components
+use App\Livewire\Backend\Admin\ProductManagement\VehicleModels;
+use App\Livewire\Backend\Admin\ProductManagement\Vehicles;
 use App\Livewire\Backend\Admin\ProductManagement\Vehicles\VehicleCreate;
 use App\Livewire\Backend\Admin\ProductManagement\Vehicles\VehicleDetails;
 use App\Livewire\Backend\Admin\ProductManagement\Vehicles\VehicleEdit;
 use App\Livewire\Backend\Admin\ProductManagement\Vehicles\VehicleList;
-
-
+use App\Livewire\Backend\Admin\ProductManagement\VehicleTransmissions;
+use App\Livewire\Backend\Admin\Profile;
+use App\Livewire\Backend\Admin\UserManagement\User;
+use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'admin', 'verified'])->name('admin.')->prefix('admin')->group(function () {
     Route::get('/dashboard', Dashboard::class)->name('dashboard');
@@ -41,19 +36,15 @@ Route::middleware(['auth', 'admin', 'verified'])->name('admin.')->prefix('admin'
         Route::get('/order/details/{id}', OrderDetails::class)->name('details');
     });
 
-
-    Route::group([], function(){
-         Route::get('/deposit-management', DepositComponent::class)->name('deposits');
-          Route::get('/deposits', DepositComponent::class)->name('deposits');
-          route::get('/deposit/details/{id}', DepositDetail::class)->name('deposit.detail');
+    Route::group([], function () {
+        Route::get('/deposit-management', DepositComponent::class)->name('deposits');
+        Route::get('/deposits', DepositComponent::class)->name('deposits');
+        route::get('/deposit/details/{id}', DepositDetail::class)->name('deposit.detail');
     });
 
-
-   
     Route::get('/orders', OrderComponent::class)->name('orders');
-  
 
-     Route::group(['prefix' => 'payment-management'], function () {
+    Route::group(['prefix' => 'payment-management'], function () {
         Route::get('/', PaymentComponent::class)->name('payments');
         Route::get('/details/{id}', PaymentDetailsComponent::class)->name('payment-details');
     });
@@ -72,4 +63,3 @@ Route::middleware(['auth', 'admin', 'verified'])->name('admin.')->prefix('admin'
         Route::get('/vehicle-details/{id}', VehicleDetails::class)->name('vehicle-details');
     });
 });
-
